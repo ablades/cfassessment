19022a2e53ce606a9eeb45f38784ecae33bb0c90
ablades <audaris@gmail.com>
2020-10-10 16:06:46 -0500

    inject avatar and name

--- index.js
+++ index.js
@@ -17,9 +17,10 @@ class LinksTransformer {
     this.links = links
   }
 
-  //incoming divs
+  // incoming divs
   async element(element) {
-    console.log(element)
+    console.log(element.tagName)
+    // inject a tags
     this.links.forEach((link, index, arr) => {
       element.append(`<a href="${link.url}">${link.name}</a>`, {html : true})
     })
@@ -30,11 +31,17 @@ async function handleRequest(request) {
   const url = new URL(request.url)
   if (url.pathname == '/links') {
     return new Response(JSON.stringify(linkData), {
-      headers: { 'content-type': 'application/json' },}
-    )} else {
-
-      const page = await fetch('https://static-links-page.signalnerve.workers.dev')
-
-      return new HTMLRewriter().on("div#links", new LinksTransformer(linkData)).transform(page)
+      headers: { 'content-type': 'application/json' },
+    })
+  } else {
+      let page = await fetch('https://static-links-page.signalnerve.workers.dev')
+      const rewriter = new HTMLRewriter()
+      
+      rewriter.on("div#links", new LinksTransformer(linkData))
+      rewriter.on("div#profile", {element: e => e.removeAttribute('style')})
+      rewriter.on("img#avatar", {element: e => e.setAttribute('src', 'https://avatars1.githubusercontent.com/u/6531016?s=60&v=4')})
+      rewriter.on("h1#name", {element: e => e.append('ablades')})
+      
+      return rewriter.transform(page)
     }
 }
