c825d83ba4454669788bc1e4ced3e7ac85bbd8dd
ablades <audaris@gmail.com>
2020-10-10 14:55:23 -0500

    add rewriter

--- index.js
+++ index.js
@@ -12,13 +12,29 @@ let linkData = [
   { "name": "Link3", "url": "https://linkurl" }
 ]
 
+class LinksTransformer {
+  constructor(links) {
+    this.links = links
+  }
+
+  //incoming divs
+  async element(element) {
+    console.log(element)
+    this.links.forEach((link, index, arr) => {
+      element.append(`<a href="${link.url}">${link.name}</a>`, {html : true})
+    })
+  }
+}
+
 async function handleRequest(request) {
   const url = new URL(request.url)
   if (url.pathname == '/links') {
     return new Response(JSON.stringify(linkData), {
-      headers: { 'content-type': 'application/json' },
-    })
-  } else {
-      return new Response('400 Bad Request', { status: 400 })
+      headers: { 'content-type': 'application/json' },}
+    )} else {
+
+      const page = await fetch('https://static-links-page.signalnerve.workers.dev')
+
+      return new HTMLRewriter().on("div#links", new LinksTransformer(linkData)).transform(page)
     }
 }
